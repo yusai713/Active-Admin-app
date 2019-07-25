bccf1583f49cb4165613e7ae6227793a31721f67
yusai13 <future.gazer7@gmail.com>
2019-07-25 22:32:58 +0900

    修正1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 User.create(name: '斉藤', age: 35, address:"okinawa")
 User.create(name: '田中', age: 21, address:"nagano")
